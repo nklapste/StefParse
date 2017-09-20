401f4bea8037c22c542b0fbc582d4b6dbbe643b5
nklapste <nklapste@ualberta.ca>
2017-09-20 17:25:04 -0600

    refactored setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
-
 from setuptools import setup, find_packages
-
 
 setup(
     name="StefParse",
     author="Nathan Klapstein",
     author_email="nklapste@ualberta.ca",
-
     version="0.0.0",
-
     description="Terminal filter/parser for AlbertaSat dev needs",
     url="https://github.com/nklapste/StefParse",
-    keywords="AlbertaSat parse filter terminal",
+    keywords="AlbertaSat parse filter logger terminal",
     packages=find_packages(exclude=["test"]),
     package_data={
-        '': ['README.md'],
+        "": ["README.md"],
     },
     install_requires=[],
     entry_points={
-        'console_scripts': ['stefparse = stefparse.__main__:main'],
+        "console_scripts": ["stefparse = stefparse.__main__:main"],
     },
 )
